ac7ee272f00fa101c692de86b2749f9752f55e41
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2020-11-15 22:49:20 -0600

    added shopkeeper with minimal interactions #3

---
+++ src/main/java/edu/missouriwestern/csmp/gg/mwsu/entities/ShopKeeper.java
@@ -0,0 +1,103 @@
+package edu.missouriwestern.csmp.gg.mwsu.entities;
+
+import edu.missouriwestern.csmp.gg.base.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class ShopKeeper extends Entity implements EventListener, Runnable {
+
+    private static Logger logger = Logger.getLogger(Guide.class.getCanonicalName());
+
+    private int waitStep = 0;
+
+    private final int MOVE_EVERY = 30;
+
+
+    public ShopKeeper(Game game, Container startingLocation) {
+        super(game, Map.of("sprites", "shopkeeper",
+                "character", "?",
+                "description", "aggenstein shopkeeper"),
+                startingLocation);
+        game.registerListener(this);
+    }
+
+    @Override
+    public void acceptEvent(Event event) {
+        switch(event.getType()) {
+            case "game-start":
+                reset(); // move to spawn point at start of game
+                break;
+            case "command":  // see if someone wants you to talk to them
+                switch(event.getProperty("command")) {
+                    case "INTERACT":
+                        logger.info("Interact was pressed");
+                        var player = getGame().getAgent(event.getProperty("username"));
+                        if (player instanceof Player) { // TODO: this cast sucks, shouldn't be tied to client, rethink approach
+                            var avatar = ((Player) player);
+                            var avatarLocation = getGame().getEntityLocation(avatar);
+                            if (avatarLocation instanceof Tile) {
+                                var tile = (Tile) avatarLocation;
+                                var board = tile.getBoard();
+                                var target = board.getAdjacentTile(tile, Direction.valueOf(event.getProperty("parameter")));
+
+                                // get nearby tiles
+                                var myLocation = getGame().getEntityLocation(this);
+                                if(!(myLocation instanceof Tile)) return;  // need to be on the map to interact
+                                var myTile = (Tile)myLocation;
+                                var nearbyTiles = new HashSet<Tile>();
+                                nearbyTiles.add(myTile);
+                                if(board.getAdjacentTile(myTile, Direction.EAST) != null)
+                                    nearbyTiles.add(board.getAdjacentTile(myTile, Direction.EAST));
+                                if(board.getAdjacentTile(myTile, Direction.WEST) != null)
+                                    nearbyTiles.add(board.getAdjacentTile(myTile, Direction.WEST));
+                                if(board.getAdjacentTile(myTile, Direction.SOUTH) != null)
+                                    nearbyTiles.add(board.getAdjacentTile(myTile, Direction.SOUTH));
+                                if(board.getAdjacentTile(myTile, Direction.NORTH) != null)
+                                    nearbyTiles.add(board.getAdjacentTile(myTile, Direction.NORTH));
+                                if (nearbyTiles.contains(target)) { // someone is interacting with us
+                                    getGame().propagateEvent(new Event(getGame(), "speech-event",
+                                            Map.of("entity", ""+this.getID(),
+                                                    "message", "what would you like to buy?",
+                                                    "responseChoices", "[\"potion\",\"sword\",\"key\"]")));
+                                }
+                            }
+                        }
+                        break;
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    public void run() {
+        waitStep++;
+        if(waitStep % MOVE_EVERY != 0) return;
+
+        logger.info("shopkeeper pacing around");
+        var direction = Direction.values()[(int) (Math.random() * 4)];
+        if ((getGame().getEntityLocation(this) instanceof Tile)) {
+            var location = (Tile) getGame().getEntityLocation(this);
+            var board = location.getBoard();
+            var destination = board.getAdjacentTile(location, direction);
+            if (destination != null && !destination.hasProperty("impassable")) {
+                destination.addEntity(this);
+            }
+        }
+    }
+
+    public void reset() {
+    }
+
+    public String getType() {
+        return "npc";
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
